da813006fb2ab51caa5d1f39f69f87d36f591d20
Andrii Nepryntsev <andriinepryntsev@gmail.com>
2025-01-26 15:11:33 +0100

    create database script for mariadb

--- database/create_database.py
+++ database/create_database.py
@@ -1,14 +1,45 @@
-import sqlite3
+import mariadb
+from subprocess import Popen, PIPE
 from dotenv import load_dotenv
 import os
 
 load_dotenv("../")
-
 ROOT_DIR = os.getenv("ROOT_DIR")
-with open(f"{ROOT_DIR}/database/db_structure.sql") as sql_file:
-    sql = sql_file.read()
+sql_file = f"{ROOT_DIR}/database/db_structure.sql"
 
-connection = sqlite3.connect(f"{ROOT_DIR}/database/almond.db")
+
+def exec_sql_file(cursor, sql_file):
+    print(f"\n[INFO] Executing SQL script file: '{sql_file}'")
+    statement = ""
+
+    try:
+        with open(sql_file, "r") as file:
+            for line in file:
+                if line.strip().startswith('--') or line.strip() == '':
+                    continue
+                if not line.strip().endswith(';'):
+                    statement += line
+                else:
+                    statement += line
+                    try:
+                        cursor.execute(statement)
+                        print(f"[INFO] Successfully executed statement:\n{
+                              statement.strip()}")
+                    except mariadb.Error as e:
+                        print(f"[WARN] MariaDB Error during execution: {e}")
+                    statement = ""
+    except FileNotFoundError as e:
+        print(f"[ERROR] SQL file not found: {e}")
+    except Exception as e:
+        print(f"[ERROR] Unexpected error: {e}")
+
+
+connection = mariadb.connect(user=os.getenv("USER"),
+                             password=os.getenv("PASSWORD"),
+                             host=os.getenv("HOST"),
+                             database=os.getenv("DATABASE"))
 cursor = connection.cursor()
-cursor.executescript(sql)
+
+exec_sql_file(cursor, sql_file)
+
 connection.close()
